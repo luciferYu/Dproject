6718503649dd4f7abf956e86746b6d4cafcdc391
yuzhiyi <81784779@qq.com>
2017-10-06 15:00:24 +0800

    创建了虚拟环境，安装了Django的安装包 ，创建了Dproject项目 下一步创建APP

---
+++ Dproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\lucifer\desktop\learnpython\python_advan\stage04\作业\day01\dproject\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
