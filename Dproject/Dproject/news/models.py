1d449ff0e3e9dced31e3cb89b91b8214d0992435
yuzhiyi <81784779@qq.com>
2017-10-07 19:59:37 +0800

    进行了增加表，级联删除，增加字段迁移等测试

--- Dproject/Dproject/news/models.py
+++ Dproject/Dproject/news/models.py
@@ -5,13 +5,43 @@ class NewsCategory(models.Model):
     '''新闻分类表'''
     def __str__(self):
         return self.cag_name
-    cag_name = models.CharField(max_length=50) #max_length指定最大长度，id默认自己会添加
+    cag_name = models.CharField(max_length=50)  # max_length指定最大长度，id默认自己会添加
 
 class NewsInfo(models.Model):
     '''定义新闻信息类'''
     def __str__(self):
         return self.news_title
 
-    news_title = models.CharField(max_length=50) #定义新闻标题
-    news_content = models.TextField(max_length=5000)#定义新闻内容
-    news_category = models.ForeignKey(NewsCategory)#定义新闻分类此处使用外键
+    news_title = models.CharField(max_length=50)  # 定义新闻标题
+    news_content = models.TextField(max_length=5000)  # 定义新闻内容
+    news_category = models.ForeignKey(NewsCategory)  # 定义新闻分类此处使用外键
+
+
+class BaseModel(models.Model):  # 创建一个基础类
+    create_time = models.DateTimeField(auto_now_add=True)  # 创建时间
+    update_time = models.DateTimeField(auto_now=True)  # 最后一次修改的时间
+
+    class Meta:  #声明当前类不是一个模型类，而是作为其他模型类的抽象基类
+        abstract = True
+
+class BookInfo(BaseModel):
+    '''图书类'''
+    book_title = models.CharField(max_length=50)  # 图书标题
+    book_comment = models.IntegerField(default=0)  # 图书评论量
+    book_read = models.IntegerField(default=0)  # 图书阅读量
+    book_delete = models.BooleanField(default=False) #图书是否被逻辑删除
+
+    def __str__(self):
+        return self.book_title
+
+
+class HeroInfo(BaseModel):
+    '''英雄类'''
+    hero_name = models.CharField(max_length=50)  # 英雄的名字
+    hero_sex = models.BooleanField(default=True)  # 英雄的性别
+    hero_desc = models.TextField()  # 英雄的描述
+    hero_delete = models.BooleanField(default=False)  # 英雄是否被逻辑删除
+    hero_book = models.ForeignKey('BookInfo')  # 英雄属于的书
+    hero_hometown = models.CharField(max_length=70,default='金庸小说') # 英雄也要问出处
+    def __str__(self):
+        return self.hero_name
--- Dproject/Dproject/news/tests.py
+++ Dproject/Dproject/news/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from .models import * #通过相对路径引入模块
+import random
 # Create your tests here.
 
 #1.插入数据
@@ -28,5 +29,71 @@ from .models import * #通过相对路径引入模块
 # print('获得一条新闻数据:'+ str(news_info))
 
 #4.删除数据
-news_info = NewsInfo.objects.get(pk=1) #先获取某条记录
-news_info.delete() #调用对象的delete()方法删除
+# news_info = NewsInfo.objects.get(pk=1) #先获取某条记录
+# news_info.delete() #调用对象的delete()方法删除
+
+# #图书数据
+# book_list = ['射雕英雄传', '天龙八部', '笑傲江湖', '雪山飞狐']
+# # 创建测试数据
+# for book_name in book_list:
+#     # 创建图书对象
+#     book = BookInfo()
+#     # 字段赋值
+#     book.book_title = book_name
+#     book.book_comment = random.randint(1, 100)
+#     book.book_read = random.randint(1, 100)
+#     book.book_delete = False
+#     # 保存图书信息
+#     book.save()
+#
+# # 英雄数据
+# hero_list = [
+#     {'name': '郭靖', 'sex': 1, 'desc': '降龙十八掌', 'book': 1},
+#     {'name': '黄蓉', 'sex': 0, 'desc': '打狗棍法', 'book': 1},
+#     {'name': '黄药师', 'sex': 1, 'desc': '弹指神通', 'book': 1},
+#     {'name': '欧阳锋', 'sex': 1, 'desc': '蛤蟆功', 'book': 1},
+#     {'name': '梅超风', 'sex': 0, 'desc': '九阴白骨爪', 'book': 1},
+#     {'name': '乔峰', 'sex': 1, 'desc': '降龙十八掌', 'book': 2},
+#     {'name': '段誉', 'sex': 1, 'desc': '六脉神剑', 'book': 2},
+#     {'name': '虚竹', 'sex': 1, 'desc': '天山六阳掌', 'book': 2},
+#     {'name': '王语嫣', 'sex': 0, 'desc': '神仙姐姐', 'book': 2},
+#     {'name': '令狐冲', 'sex': 1, 'desc': '独孤九剑', 'book': 3},
+#     {'name': '任盈盈', 'sex': 0,'desc': '弹琴', 'book': 3},
+#     {'name': '岳不群', 'sex': 1, 'desc': '华山剑法', 'book': 3},
+#     {'name': '东方不败', 'sex': 1, 'desc': '葵花宝典', 'book': 3},
+#     {'name': '胡斐', 'sex': 1, 'desc': '胡家刀法', 'book': 4},
+#     {'name': '苗若兰', 'sex': 0, 'desc': '黄衣', 'book': 4},
+#     {'name': '程灵素', 'sex': 0, 'desc': '医术', 'book': 4},
+#     {'name': '袁紫衣', 'sex': 0, 'desc': '六合拳', 'book': 4},
+# ]
+#
+# # 创建测试数据
+# for hero_info in hero_list:
+#     # 创建英雄类
+#     hero = HeroInfo()
+#     # 字段赋值
+#     hero.hero_name = hero_info['name']
+#     hero.hero_desc = hero_info['desc']
+#     hero.hero_sex = hero_info['sex']
+#     hero.hero_delete = False
+#     hero.hero_book_id = hero_info['book']
+#     # 保存对象
+#     hero.save()
+
+
+
+
+# hero =  HeroInfo.objects.get(pk=1)
+# print(hero, hero.hero_book)
+#
+#
+# book = BookInfo.objects.get(pk=1)
+#
+# hero_list = book.heroinfo_set.all()
+# print(hero_list)
+
+
+# book = BookInfo.objects.get(pk=3)  # 获得一本书
+# book.delete()  # 级联删除
+
+
